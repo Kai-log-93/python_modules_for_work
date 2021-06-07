66a46450b45adb801f1ed4132e639ae322417edd
Kai <kyzendom@gmail.com>
2021-06-07 20:31:44 +0800

    udpate

--- openpyxl/format.py
+++ openpyxl/format.py
@@ -1,3 +1,4 @@
+from openpyxl import styles
 from openpyxl.styles import Font,colors,Alignment,PatternFill, GradientFill, Border, Side
 from openpyxl.styles import NamedStyle
 from openpyxl.workbook import Workbook
@@ -16,7 +17,7 @@ ws.merge_cells(start_row=2, start_column=2, end_row=5, end_column=5)
 
 # set cell font style
 cell = ws['B2']
-cell.font = Font(color='FF000000', size=20, italic=True)
+cell.font = Font(color='FF0000', size=20, italic=True)
 # set cell value
 cell.value = 'Merged Cell'
 # set cell alignment
@@ -24,6 +25,21 @@ cell.alignment = Alignment(horizontal='right', vertical='bottom')
 # GradientFill
 cell.fill = GradientFill(stop=('000000', 'FFFFFF'))
 wb.save('text.xlsx')
+
+highlight = NamedStyle(name='hightlight')
+highlight.font = Font(bold=True)
+bd = Side(style='thick', color='000000')
+highlight.border = Border(left=bd,top=bd,right=bd,bottom=bd)
+highlight.fill = PatternFill('solid',fgColor='FFFF00')
+
+count = 0
+for col in ws.iter_cols(min_col=8, min_row=1, max_col=30, max_row=30):
+    col[count].style = highlight
+    count += 1
+
+wb.save('hightlight.xlsx')
+
+
 
 
 
